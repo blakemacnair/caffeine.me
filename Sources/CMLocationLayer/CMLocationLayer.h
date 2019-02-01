d8671fe1f646af9e64fac2c4ee73b3c593a1dc41
Blake Macnair <blakemacnair@gmail.com>
2019-01-31 22:44:52 -0600

    Add separate frameworks for view logic, location logic, and FourSquare logic.

---
+++ Sources/CMLocationLayer/CMLocationLayer.h
@@ -0,0 +1,19 @@
+//
+//  CMLocationLayer.h
+//  CMLocationLayer
+//
+//  Created by Blake Macnair on 1/31/19.
+//  Copyright © 2019 Blake Macnair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CMLocationLayer.
+FOUNDATION_EXPORT double CMLocationLayerVersionNumber;
+
+//! Project version string for CMLocationLayer.
+FOUNDATION_EXPORT const unsigned char CMLocationLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMLocationLayer/PublicHeader.h>
+
+
