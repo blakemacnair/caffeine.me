8de27ff1c3b0d3775001dbac4e4eb8f9607ec3b7
Blake Macnair <blakemacnair@gmail.com>
2019-02-04 22:49:22 -0600

    Add implementation for location services.

--- Sources/CMLocationLayer/CMLocationRelay.swift
+++ Sources/CMLocationLayer/CMLocationRelay.swift
@@ -13,9 +13,8 @@ import CoreLocation
 
 public protocol CMLocationRelayProtocol {
     var authStatus: Observable<CLAuthorizationStatus> { get }
-    var locationServicesEnabled: Observable<Bool> { get }
     var placemark: Observable<CLPlacemark> { get }
-    var heading: Observable<CLHeading> { get }
+    var heading: Observable<CLHeading?> { get }
 
     func requestAuthorization() -> Observable<CLAuthorizationStatus>
 
@@ -23,9 +22,64 @@ public protocol CMLocationRelayProtocol {
     func stopUpdatingLocation()
 
     func startUpdatingHeading() -> Bool
-    func stopUpdatingHeading() -> Bool
+    func stopUpdatingHeading()
 }
 
 public final class CMLocationRelay: CMLocationRelayProtocol {
 
+    // MARK: - Properties
+
+    public let authStatus: Observable<CLAuthorizationStatus>
+    private let authStatusRelay = BehaviorRelay<CLAuthorizationStatus>(value: .notDetermined)
+
+    public let placemark: Observable<CLPlacemark>
+
+    public let heading: Observable<CLHeading?>
+
+    private let disposeBag = DisposeBag()
+
+    private let manager: CLLocationManager
+
+    // MARK: - Init
+
+    public init() {
+        manager = CLLocationManager()
+
+        authStatus = authStatusRelay.asObservable()
+        manager.rx
+            .didChangeAuthorization
+            .map { $0.status }
+            .bind(to: authStatusRelay)
+            .disposed(by: disposeBag)
+
+        placemark = manager.rx.placemark
+        heading = manager.rx.heading
+    }
+
+    // MARK: - Public
+
+    public func requestAuthorization() -> Observable<CLAuthorizationStatus> {
+        manager.requestWhenInUseAuthorization()
+        return self.authStatus
+    }
+
+    public func startUpdatingLocation() -> Bool {
+        guard CLLocationManager.locationServicesEnabled() else { return false }
+        manager.startUpdatingLocation()
+        return true
+    }
+
+    public func stopUpdatingLocation() {
+        manager.stopUpdatingLocation()
+    }
+
+    public func startUpdatingHeading() -> Bool {
+        guard CLLocationManager.headingAvailable() else { return false }
+        manager.startUpdatingHeading()
+        return true
+    }
+
+    public func stopUpdatingHeading() {
+        manager.stopUpdatingHeading()
+    }
 }
