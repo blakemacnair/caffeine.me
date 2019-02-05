7b9a94a2fc1697c2bcf51c3b146984a44a98c61e
Blake Macnair <blakemacnair@gmail.com>
2019-02-04 22:57:04 -0600

    Add some test code to the app coord for location services.

--- Sources/CMViewLayer/Base/AppCoordinator.swift
+++ Sources/CMViewLayer/Base/AppCoordinator.swift
@@ -14,6 +14,8 @@ public final class AppCoordinator: BaseCoordinator<Void> {
     private let window: UIWindow
     private let navigationController = UINavigationController()
 
+    private let locationRelay: CMLocationRelayProtocol = CMLocationRelay()
+
     public init(window: UIWindow) {
         self.window = window
     }
@@ -27,6 +29,26 @@ public final class AppCoordinator: BaseCoordinator<Void> {
         let sampleVC = UIViewController()
         sampleVC.view.backgroundColor = .magenta
         navigationController.setViewControllers([sampleVC], animated: false)
+
+        locationRelay.requestAuthorization()
+            .debug("AUTH STATUS")
+            .filter { auth in
+                guard case .authorizedWhenInUse = auth else { return false }
+                return true
+            }
+            .subscribe(onNext: { [unowned self] auth in
+                self.locationRelay.startUpdatingLocation()
+                self.locationRelay.startUpdatingHeading()
+            })
+            .disposed(by: self.disposeBag)
+
+        locationRelay.placemark.debug("PLACEMARK")
+            .subscribe()
+            .disposed(by: disposeBag)
+
+        locationRelay.heading.debug("HEADING")
+            .subscribe()
+            .disposed(by: disposeBag)
 
         return Signal.never()
     }
