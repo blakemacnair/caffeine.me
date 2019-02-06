cc8eb3c583ff9aa9ab33c57145a8b1501b8dd7c7
Blake Macnair <blakemacnair@gmail.com>
2019-02-06 17:40:34 -0600

    Set up the map coordinator and start it from the app coordinator.

--- Sources/CMViewLayer/Base/AppCoordinator.swift
+++ Sources/CMViewLayer/Base/AppCoordinator.swift
@@ -7,17 +7,17 @@ import RxSwift
 import RxCocoa
 import UIKit
 
-import CMLocationLayer
-
 public final class AppCoordinator: BaseCoordinator<Void> {
 
     private let window: UIWindow
     private let navigationController = UINavigationController()
 
-    private let locationRelay: CMLocationRelayProtocol = CMLocationRelay()
+    private let mapCoordinator: MapCoordinator
 
     public init(window: UIWindow) {
         self.window = window
+
+        self.mapCoordinator = MapCoordinator(navigationController: self.navigationController)
     }
 
     public override func start() -> Signal<Void> {
@@ -25,30 +25,8 @@ public final class AppCoordinator: BaseCoordinator<Void> {
 
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
-        
-        let sampleVC = UIViewController()
-        sampleVC.view.backgroundColor = .magenta
-        navigationController.setViewControllers([sampleVC], animated: false)
 
-        locationRelay.requestAuthorization()
-            .debug("AUTH STATUS")
-            .filter { auth in
-                guard case .authorizedWhenInUse = auth else { return false }
-                return true
-            }
-            .subscribe(onNext: { [unowned self] auth in
-                self.locationRelay.startUpdatingLocation()
-                self.locationRelay.startUpdatingHeading()
-            })
-            .disposed(by: self.disposeBag)
-
-        locationRelay.placemark.debug("PLACEMARK")
-            .subscribe()
-            .disposed(by: disposeBag)
-
-        locationRelay.heading.debug("HEADING")
-            .subscribe()
-            .disposed(by: disposeBag)
+        _ = mapCoordinator.start()
 
         return Signal.never()
     }
--- Sources/CMViewLayer/Map/MapCoordinator.swift
+++ Sources/CMViewLayer/Map/MapCoordinator.swift
@@ -6,6 +6,23 @@
 //  Copyright © 2019 Blake Macnair. All rights reserved.
 //
 
+import RxSwift
+import RxCocoa
+import RxMKMapView
+
 final class MapCoordinator: BaseCoordinator<Void> {
 
+    private let navigationController: UINavigationController
+    private let viewController: UIViewController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.viewController = UIViewController()
+        self.viewController.view.backgroundColor = .yellow
+    }
+
+    override func start() -> Signal<Void> {
+        navigationController.pushViewController(self.viewController, animated: true)
+        return .never()
+    }
 }
