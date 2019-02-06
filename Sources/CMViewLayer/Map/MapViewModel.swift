9cb2cc47d86719db8a9243a0be59c702e30370e6
Blake Macnair <blakemacnair@gmail.com>
2019-02-06 17:32:52 -0600

    Add basic protocol outline for map view model.

--- Sources/CMViewLayer/Map/MapViewModel.swift
+++ Sources/CMViewLayer/Map/MapViewModel.swift
@@ -6,6 +6,23 @@
 //  Copyright © 2019 Blake Macnair. All rights reserved.
 //
 
+import RxSwift
+import RxCocoa
+
+enum MapViewState {
+    case loading
+    case ready
+}
+
+enum MapViewAction {
+    case viewLoaded
+}
+
+protocol MapViewModelProtocol {
+    var actions: PublishRelay<MapViewAction> { get }
+    var state: Driver<MapViewState> { get }
+}
+
 struct MapViewModel {
     
 }
