49df2840f499b7516bed0c2ebf235ee5c2678a88
Blake Macnair <blakemacnair@gmail.com>
2019-02-06 17:27:55 -0600

    Add some basic protocol logic for the interactor level.

--- Sources/CMViewLayer/Map/MapInteractor.swift
+++ Sources/CMViewLayer/Map/MapInteractor.swift
@@ -6,6 +6,26 @@
 //  Copyright © 2019 Blake Macnair. All rights reserved.
 //
 
+import RxSwift
+import RxCocoa
+
+import CMLocationLayer
+import class CoreLocation.CLPlacemark
+import class CoreLocation.CLHeading
+
+enum MapInteractorState: Equatable {
+    case loading
+    case locationServicesNotAuthorized
+    case locationServicesUnavailable
+    case ready(userPlacemark: CLPlacemark, userHeading: CLHeading)
+}
+
+protocol MapInteractorProtocol {
+    var state: BehaviorRelay<MapInteractorState> { get }
+
+    func startLocationServies()
+}
+
 final class MapInteractor {
-    
+
 }
