b809138c47816829f7d3457ed3086f25dc9e2f6a
Blake Macnair <blakemacnair@gmail.com>
2019-02-06 20:33:26 -0600

    Add empty test files for testing codability of new structs.

--- Sources/CMFourSquareLayer/CMFourSquareLayerTests/Models/Response/VenueSearchResponseTests.swift
+++ Sources/CMFourSquareLayer/CMFourSquareLayerTests/Models/Response/VenueSearchResponseTests.swift
@@ -1,5 +1,5 @@
 //
-//  CMFourSquareLayerTests.swift
+//  VenueSearchResponseTests.swift
 //  CMFourSquareLayerTests
 //
 //  Created by Blake Macnair on 2/6/19.
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class CMFourSquareLayerTests: XCTestCase {
+class VenueSearchResponseTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
