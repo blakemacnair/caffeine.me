38282d13e1380b664c651fd1f1395963c72738df
Blake Macnair <blakemacnair@gmail.com>
2019-02-06 18:01:57 -0600

    Set up an example usage of the map view.

--- Sources/CMViewLayer/Map/MapCoordinator.swift
+++ Sources/CMViewLayer/Map/MapCoordinator.swift
@@ -9,6 +9,7 @@
 import RxSwift
 import RxCocoa
 import RxMKMapView
+import MapKit
 
 final class MapCoordinator: BaseCoordinator<Void> {
 
@@ -18,7 +19,30 @@ final class MapCoordinator: BaseCoordinator<Void> {
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
         self.viewController = UIViewController()
-        self.viewController.view.backgroundColor = .yellow
+
+        super.init()
+
+        let view = MKMapView()
+        viewController.view = view
+
+        let chicagoLocation = CLLocationCoordinate2D(latitude: 41.8781,
+                                                     longitude: -87.6298)
+
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let chicagoRegion = MKCoordinateRegion(center: chicagoLocation, span: span)
+
+        view.setRegion(chicagoRegion, animated: true)
+
+        let chicagoAnnotation = MKPointAnnotation()
+        chicagoAnnotation.coordinate = chicagoLocation
+        chicagoAnnotation.title = "Chicago"
+        chicagoAnnotation.subtitle = "Illinois"
+
+        let annotations = Observable<[MKAnnotation]>.just([chicagoAnnotation])
+
+        annotations.asDriver(onErrorJustReturn: [])
+            .drive(view.rx.annotations)
+            .disposed(by: self.disposeBag)
     }
 
     override func start() -> Signal<Void> {
