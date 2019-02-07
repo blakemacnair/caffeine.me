07354696077bf0dfb308ffd21684494eb2f23425
Blake Macnair <blakemacnair@gmail.com>
2019-02-06 18:39:03 -0600

    Move the VC sampl logic into the VC class.

--- Sources/CMViewLayer/Map/MapCoordinator.swift
+++ Sources/CMViewLayer/Map/MapCoordinator.swift
@@ -14,35 +14,13 @@ import MapKit
 final class MapCoordinator: BaseCoordinator<Void> {
 
     private let navigationController: UINavigationController
-    private let viewController: UIViewController
+    private let viewController: MapViewController
 
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
-        self.viewController = UIViewController()
+        self.viewController = MapViewController()
 
         super.init()
-
-        let view = MKMapView()
-        viewController.view = view
-
-        let chicagoLocation = CLLocationCoordinate2D(latitude: 41.8781,
-                                                     longitude: -87.6298)
-
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        let chicagoRegion = MKCoordinateRegion(center: chicagoLocation, span: span)
-
-        view.setRegion(chicagoRegion, animated: true)
-
-        let chicagoAnnotation = MKPointAnnotation()
-        chicagoAnnotation.coordinate = chicagoLocation
-        chicagoAnnotation.title = "Chicago"
-        chicagoAnnotation.subtitle = "Illinois"
-
-        let annotations = Observable<[MKAnnotation]>.just([chicagoAnnotation])
-
-        annotations.asDriver(onErrorJustReturn: [])
-            .drive(view.rx.annotations)
-            .disposed(by: self.disposeBag)
     }
 
     override func start() -> Signal<Void> {
--- Sources/CMViewLayer/Map/MapViewController.swift
+++ Sources/CMViewLayer/Map/MapViewController.swift
@@ -6,6 +6,45 @@
 //  Copyright © 2019 Blake Macnair. All rights reserved.
 //
 
+import RxSwift
+
+import MapKit
+import RxMKMapView
+
 final class MapViewController: UIViewController {
 
+    private let rootView = MKMapView()
+
+    private let disposeBag = DisposeBag()
+
+    convenience init() {
+        self.init(nibName: nil, bundle: nil)
+    }
+
+    override func loadView() {
+        self.view = rootView
+    }
+
+    override func viewDidLoad() {
+        let chicagoLocation = CLLocationCoordinate2D(latitude: 41.8781,
+                                                     longitude: -87.6298)
+
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let chicagoRegion = MKCoordinateRegion(center: chicagoLocation, span: span)
+
+        rootView.setRegion(chicagoRegion, animated: true)
+
+        let chicagoAnnotation = MKPointAnnotation()
+        chicagoAnnotation.coordinate = chicagoLocation
+        chicagoAnnotation.title = "Chicago"
+        chicagoAnnotation.subtitle = "Illinois"
+
+        rootView.setUserTrackingMode(.follow, animated: true)
+
+        let annotations = Observable<[MKAnnotation]>.just([chicagoAnnotation])
+
+        annotations.asDriver(onErrorJustReturn: [])
+            .drive(rootView.rx.annotations)
+            .disposed(by: self.disposeBag)
+    }
 }
