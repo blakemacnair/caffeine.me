41e0970469a5931349745140d5c03311c87e428b
Blake Macnair <blakemacnair@gmail.com>
2019-02-06 20:10:07 -0600

    Add struct for JSON payloads ddescribing venues.

---
+++ Sources/CMFourSquareLayer/Models/Venue.swift
@@ -0,0 +1,35 @@
+//
+//  Venue.swift
+//  CMFourSquareLayer
+//
+//  Created by Blake Macnair on 2/6/19.
+//  Copyright © 2019 Blake Macnair. All rights reserved.
+//
+
+public struct Location: Equatable & Codable {
+    public let address: String
+    public let lat: Double
+    public let lng: Double
+}
+
+public struct Category: Equatable & Codable {
+    internal let id: String
+    public let name: String
+    public let pluralName: String
+    public let shortName: String
+    public let primary: Bool
+}
+
+public struct Venue: Equatable & Codable {
+    internal let id: String
+    public let name: String
+    public let distance: Int
+    public let postalCode: String
+    public let countryCode: String
+    public let state: String
+    public let city: String
+    public let formattedAddress: [String]
+
+    public let location: Location
+    public let categories: [Category]
+}
