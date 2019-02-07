383a8a5a3f90c219df6e8cfaada6bbaf0e698bb4
Blake Macnair <blakemacnair@gmail.com>
2019-02-06 21:55:18 -0600

    Fix location and venue values.

--- Sources/CMFourSquareLayer/Models/Venue.swift
+++ Sources/CMFourSquareLayer/Models/Venue.swift
@@ -7,20 +7,21 @@
 //
 
 public struct Location: Equatable & Codable {
-    public let address: String
-    public let lat: Double
-    public let lng: Double
+    public let address: String?
+    public let lat: Double?
+    public let lng: Double?
+    public let distance: Int?
+    public let postalCode: String?
+    public let cc: String?
+    public let country: String?
+    public let state: String?
+    public let city: String?
+    public let formattedAddress: [String]
 }
 
 public struct Venue: Equatable & Codable {
     internal let id: String
     public let name: String
-    public let distance: Int
-    public let postalCode: String
-    public let countryCode: String
-    public let state: String
-    public let city: String
-    public let formattedAddress: [String]
 
     public let location: Location
     public let categories: [Category]
