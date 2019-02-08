e550a6c946500bc06c7eb87e3660df8754e9c8de
Blake Macnair <blakemacnair@gmail.com>
2019-02-07 23:41:50 -0600

    Add empty files for the venue detail view.

---
+++ Sources/CMViewLayer/VenueDetail/VenueDetailInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  VenueDetailInteractor.swift
+//  CMViewLayer
+//
+//  Created by Blake Macnair on 2/7/19.
+//  Copyright © 2019 Blake Macnair. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/CMViewLayer/VenueDetail/VenueDetailViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  VenueDetailViewModel.swift
+//  CMViewLayer
+//
+//  Created by Blake Macnair on 2/7/19.
+//  Copyright © 2019 Blake Macnair. All rights reserved.
+//
+
+import Foundation
