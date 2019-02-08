a0500c7a6dd6fed3bf0caa1efcc440ed4b3d5f1e
Blake Macnair <blakemacnair@gmail.com>
2019-02-08 00:16:31 -0600

    Throw together a rudimentary venue detail view controller / view.

--- Sources/CMViewLayer/VenueDetail/VenueDetailViewController.swift
+++ Sources/CMViewLayer/VenueDetail/VenueDetailViewController.swift
@@ -8,6 +8,7 @@
 
 import RxSwift
 import RxCocoa
+import SnapKit
 
 import struct CMFourSquareLayer.Venue
 
@@ -17,6 +18,58 @@ protocol VenueDetailViewControllerProtocol {
 }
 
 final class VenueDetailViewController: UIViewController, VenueDetailViewControllerProtocol {
+
+    // MARK: - Properties
+
+    private let disposeBag = DisposeBag()
+
+    private let rootView = VenueDetailView()
+
     let state = BehaviorRelay<VenueDetailViewState>(value: .loading)
     let uiEvents = PublishRelay<VenueDetailViewAction>()
+
+    // MARK: = Public
+
+    public override func loadView() {
+        self.view = rootView
+    }
+}
+
+final class VenueDetailView: UIView {
+
+    // MARK: - Properties
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        label.text = "VENUE"
+        label.font = .preferredFont(forTextStyle: .title2)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+
+        return label
+    }()
+
+    // MARK: - Public
+
+    convenience init() {
+        self.init(frame: .zero)
+        setup()
+    }
+
+    // MARK: - Private
+
+    private func setup() {
+        backgroundColor = .white
+        addSubview(titleLabel)
+
+        activateInitialConstraints()
+    }
+
+    private func activateInitialConstraints() {
+        titleLabel.snp.makeConstraints { mk in
+            mk.top.left.right.equalTo(self).inset(8)
+        }
+    }
 }
